2da9a2fc6c32112dcb51ff89591de935ea5c6c6c
Borja940 <2017-008734@intecap.edu.gt>
2026-09-21 22:28:33 -0600

    Primer Commit, se crearon vistas

---
+++ src/main/java/com/mycompany/vetcontrol/VetControl.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.vetcontrol;
+
+/**
+ *
+ * @author andy-
+ */
+public class VetControl {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
